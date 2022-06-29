24493a02e05d1f98376f62efb2c4d4972ca5f6ec
magicaldave <smokydave@icloud.com>
2022-06-29 12:25:21 -0500

    Add: main.c test

--- main.c
+++ main.c
@@ -1,5 +1,36 @@
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
 
 int main(void)
 {
-	return (1);
+	_printf("%i", 1024);
+	return (0);
+}
+
+int _printf(const char *format, ...)
+{
+	int integer;
+	unsigned int i = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+			case 'i':
+				integer = va_arg(args, int);
+				write(1, &integer, 4);
+				break;
+			}
+
+		}
+		i++;
+	}
+	return (0);
 }

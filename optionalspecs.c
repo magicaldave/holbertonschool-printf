ff72b17396ffd8ae8372c6681013926dd98555f9
magicaldave <smokydave@icloud.com>
2022-07-09 23:54:43 -0500

    Edit: optionalspecs.c, migrated to one large loop which handles all character instances.

--- optionalspecs.c
+++ optionalspecs.c
@@ -8,33 +8,33 @@
 int print_realstring(va_list args, char *bigbuf)
 {
 	char *s, *invalid;
-	int len, loc;
+	int len = 0, loc;
 
 	v_init(s, char *); /* va_arg + strlen(bigbuf) */
 
 	if (!s)
 		s = "(null)";
 
-	for (len = 0; *s >= 32 && *s < 127 ; )
+
+	for (len = 0; *s ; s++)
 	{
-		bigbuf[loc] = *s;
-		s++;
-		loc++;
-		len++;
-	}
-
-	for (; *s && (*s < 32 || *s >= 127); s++, len += 3)
-	{
-		bigbuf[loc] = '\\';
-		bigbuf[loc + 1] = 'x';
-		bigbuf[loc + 2] = '0';
-
-		invalid = convert(*s, 16);
-
-		for (loc += 3; *invalid ; invalid++, loc++, len++)
+		if (*s < 7 || (*s < 32 && *s > 13) || *s > 126)
 		{
-			bigbuf[loc] = *invalid;
+			bigbuf[loc] = '\\';
+			bigbuf[loc + 1] = 'x';
+			bigbuf[loc + 2] = '0';
+
+			invalid = convert(*s, 16);
+
+			for (loc += 3, len += 3; *invalid ; invalid++, loc++, len++)
+			{
+				bigbuf[loc] = *invalid;
+			}
+			continue;
 		}
+		bigbuf[loc] = *s;
+		len++;
+		loc++;
 	}
 
 	return (len);

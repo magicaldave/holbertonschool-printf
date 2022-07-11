1f355bf0d4dddb65ef32adcb4ce05262e4cb61ee
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-11 13:13:32 -0700

    macros in their own header files

--- main.h
+++ main.h
@@ -2,34 +2,7 @@
 #define MAIN_H
 
 /* MACROS */
-#define startprintf do {				\
-		va_start(args, format);			\
-		bigbuf = calloc(2048, sizeof(char));	\
-	} while (0)
-
-#define v_init(var, type) do {				\
-			loc = _strlen(bigbuf);		\
-			var = va_arg(args, type);	\
-		} while (0)
-
-#define writestrtobuf(ptr) for (; *ptr ; bigbuf[loc] = *ptr, \
-					ptr++,			    \
-					loc++,			    \
-					len++)
-
-#define writetwochars do {			\
-		bigbuf[c] = *(format - 1);	\
-		bigbuf[c + 1] = *format;	\
-		c += 2 ;			\
-	} while (0)
-
-#define endprintf do {				\
-		write(1, bigbuf, c);		\
-		va_end(args);			\
-		free(bigbuf);			\
-	} while (0)
-
-#define ABS(x) ((x) * (((x) > 0) - ((x) < 0)))
+#include "macro.h"
 
 /* Standard Libraries */
 #include <stdio.h>
